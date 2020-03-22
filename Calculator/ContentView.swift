9ade763a1178eb7e0c3473dba4383cdf135c5073
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-03-22 15:39:25 -0400

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Brandon Potts on 3/22/20.
+//  Copyright © 2020 Brandon Potts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
