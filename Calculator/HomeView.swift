529b83bc43aca25b6ff053f334f2b8c872bad138
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-03-28 13:50:12 -0400

    added the rest of the fields to the calc app

--- Calculator/HomeView.swift
+++ Calculator/HomeView.swift
@@ -16,8 +16,55 @@ struct HomeView: View {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             VStack{
+                Text(String(total))
+                    .foregroundColor(Color.white)
                 HStack {
+                    Button("AC", action: {
+                        total += 0
+                    }).background(Color.gray)
+                    Button("+/-", action: {
+                        total += 0
+                    }).background(Color.gray)
+                    Button("%", action: {
+                        total += 0
+                    }).background(Color.gray)
+                    Button("/", action: {
+                        total += 0
+                    }).background(Color.yellow)
+                }
+                HStack {
+                    Button("7", action: {
+                        total += 7
+                    }).background(Color.gray)
                     
+                    Button("8", action: {
+                        total += 1
+                    }).background(Color.gray)
+                    
+                    Button("9", action: {
+                        total += 1
+                    }).background(Color.gray)
+                    Button("x", action: {
+                        total = 30
+                    }).background(Color.yellow)
+                }
+                HStack {
+                    Button("4", action: {
+                        total += 4
+                    }).background(Color.gray)
+                    
+                    Button("5", action: {
+                        total += 5
+                    }).background(Color.gray)
+                    
+                    Button("6", action: {
+                        total += 6
+                    }).background(Color.gray)
+                    Button("-", action: {
+                        total = 30
+                    }).background(Color.yellow)
+                }
+                HStack {
                     Button("1", action: {
                         total += 1
                     }).background(Color.gray)
