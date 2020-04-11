7e945ec0dd5fb0a4cd2540136d3e3aab8a193e7b
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-04-11 14:48:06 -0400

    implemented CalcButtonView.swift

--- Calculator/Buttons/CalcButton.swift
+++ Calculator/Buttons/CalcButton.swift
@@ -15,50 +15,22 @@ import SwiftUI
 struct CalcButton: Hashable, Identifiable{
     
     let id = UUID()
-    let highlightAble: Bool
-    let originalBackgroundColor: Color
     var label: String
-    var backgroundColor: Color
-    var foregroundColor: Color
     var action: ()?
     
 
-    init(label: String, backgroundColor: Color = Color.darkGray, highlightAble: Bool = false, action: ()? = nil) {
+    init(label: String, action: ()? = nil) {
         self.label = label
-        self.backgroundColor = backgroundColor
-        self.highlightAble = highlightAble
-        self.originalBackgroundColor = backgroundColor
-        self.foregroundColor = Color.white
         self.action = action
     }
     
     static func == (lhs: CalcButton, rhs: CalcButton) -> Bool {
-        return lhs.label == rhs.label && lhs.backgroundColor == rhs.backgroundColor
+        return lhs.label == rhs.label && lhs.id == rhs.id
     }
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(label)
-        hasher.combine(backgroundColor)
+        hasher.combine(id)
     }
     
-    mutating func checkForHightlight() -> Void {
-        if self.highlightAble {
-            // Will be true when the button isn't already highlighted
-            if self.backgroundColor == self.originalBackgroundColor {
-                self.highlightOn()
-            } else {
-                self.highlightOff()
-            }
-        }
-    }
-    
-    mutating func highlightOn() -> Void {
-        self.foregroundColor = Color.orange
-        self.backgroundColor = Color.white
-    }
-    
-    mutating func highlightOff() -> Void {
-        self.backgroundColor = self.originalBackgroundColor
-        self.foregroundColor = Color.white
-    }
 }
---
+++ Calculator/Views/CalcButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  CalcButtonView.swift
+//  Calculator
+//
+//  Created by Brandon Potts on 4/11/20.
+//  Copyright © 2020 Brandon Potts. All rights reserved.
+//
+
+import SwiftUI
+
+struct CalcButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalcButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalcButtonView()
+    }
+}
